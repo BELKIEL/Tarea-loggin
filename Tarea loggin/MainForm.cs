4c8dead6e36586ba80cd6ff9d419f6ba556be888
belkiel <emmanuel_16192@hotmail.com>
2014-05-05 00:11:13 -0500

    se eliminaron comentarios

--- Tarea loggin/MainForm.cs
+++ Tarea loggin/MainForm.cs
@@ -6,26 +6,20 @@ using System.Windows.Forms;
 
 namespace Tarea_loggin
 {
-	/// <summary>
-	/// Description of MainForm.
-	/// </summary>
+	
 	public partial class MainForm : Form
 	{
 		public MainForm()
 		{
-			//
-			// The InitializeComponent() call is required for Windows Forms designer support.
-			//
+			
 			InitializeComponent();
 			
-			//
-			// TODO: Add constructor code after the InitializeComponent() call.
-			//
+			
 		}
 		
 		void Button1Click(object sender, EventArgs e)
 		{
-		System.Windows.Forms.MessageBox.Show("Ingresando");
+		System.Windows.Forms.MessageBox.Show("Validando contraseña");
 		}
 
 		
